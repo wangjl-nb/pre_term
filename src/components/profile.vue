917d10593cb25fca09b07524d4ed8fa73c41cf17
monkeyhjy <1246374291@qq.com>
2020-08-12 20:11:24 +0800

    修改了个人信息的路由传值代码

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -19,7 +19,7 @@
 										</div></el-col>
 										<el-col :span="24"><div class="grid-content">
 											<span>密码：</span>
-											<el-button type="text" @click="$router.push('/diamond/profile/changepassword')">修改密码</el-button>
+											<el-button type="text" @click="$router.push({name: 'ChangePassword', params: {oldpwd: oldpwd}})">修改密码</el-button>
 										</div></el-col>
 										<el-col :span="24"><div class="grid-content" style="margin-top: 0.4rem">
 											<span>邮箱：</span>
@@ -32,37 +32,6 @@
 							<el-footer></el-footer>
 						</el-container>
 					</el-col>
-					<el-col :span="8"><div class="grid-content">
-						<div>
-							<el-button type="primary" size="small" @click="commentVisible = true">评论</el-button>
-
-							<el-dialog
-											title="请发布评论"
-											:visible.sync="commentVisible"
-											width="30%"
-											center>
-								<el-form ref="form" :model="form" label-width="80px">
-									<el-form-item label="评论">
-										<el-input v-model="form.comment"></el-input>
-									</el-form-item>
-								</el-form>
-								<span slot="footer" class="dialog-footer">
-          <el-button native-type="submit" type="primary" @click="onSubmit">确 定</el-button>
-          <el-button @click="commentVisible = false">取 消</el-button>
-        </span>
-							</el-dialog>
-						</div>
-						<div>
-							<input type="text" v-model="url" style="display: none">
-							<el-button class="copyURL"
-												 icon="el-icon-share"
-												 :data-clipboard-text="url"
-												 size="small"
-												 type="info"
-												 @click="copy">
-								分享链接</el-button>
-						</div>
-					</div></el-col>
 				</el-row>
 			</el-main>
 		</el-container>
@@ -74,84 +43,11 @@
 	export default {
 		name: 'Profile',
 		data() {
-			let validateUsername = (rule, value, callback) => {
-				if (value === '') {
-					callback(new Error('请输入用户名'));
-				} else {
-					if (this.ruleForm.username !== '') {
-						this.$refs.ruleForm.validateField('checkPass');
-					}
-					callback();
-				}
-			};
-			let validateOldPassword = (rule, value, callback) => {
-				if (value === '') {
-					callback(new Error('请输入原密码'));
-				} else if (value !== this.oldpwd) {
-					callback(new Error('原密码错误'))
-				} else {
-					callback();
-				}
-			};
-			let validatePass = (rule, value, callback) => {
-				if (value === '') {
-					callback(new Error('请输入新密码'));
-				} else {
-					if (this.ruleForm.checkPassword !== '') {
-						this.$refs.ruleForm.validateField('checkPassword');
-					}
-					callback();
-				}
-			};
-			let validatePass2 = (rule, value, callback) => {
-				if (value === '') {
-					callback(new Error('请再次输入密码'));
-				} else if (value !== this.ruleForm.password) {
-					callback(new Error('两次输入密码不一致!'));
-				} else {
-					callback();
-				}
-			};
 			return {
 				dialogImageUrl: "",
-				imageVisible: false,
-				usernameVisible: false,
-				passwordVisible: false,
-				oldpwd: '',
+				oldpwd: '12345',
 				circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
-
 				upload_url: 'upload/pic',
-				ruleForm: {
-					ad_url: '',//上传后的图片或视频URL
-					username: '',//用户名
-					image:[] ,
-					oldPassword: '',
-					password: '',
-					checkPassword: '',
-					email: '',
-				},
-				rules: {
-					username: [
-						{ validator: validateUsername, trigger: 'blur'}
-					],
-					oldPassword: [
-						{ validator: validateOldPassword, trigger: 'blur' }
-					],
-					password: [
-						{ validator: validatePass, trigger: 'blur' }
-					],
-					checkPassword: [
-						{ validator: validatePass2, trigger: 'blur' }
-					],
-				},
-
-
-
-				commentVisible: false,
-				form: {
-					comment: ''
-				},
-				url: '',
 			};
 		},
 
@@ -168,69 +64,13 @@
 					console.log(res);
 					that.ruleForm.image.push({name:"头像",url:res.data.img});
 					that.ruleForm.username=res.data.username
-					this.ruleForm.password=res.data.password
 					that.oldpwd = res.data.password
 					that.ruleForm.email=res.data.email
 				}
 			})
-
-			this.getURL()
 		},
 		methods: {
 			//图片上传之前检验
-			beforeImageUpload(file) {
-				console.log(file)
-				var testmsg=file.name.substring(file.name.lastIndexOf('.')+1)
-				const isJpg = testmsg === 'jpg' || testmsg === 'png'
-				if (!isJpg) {
-					this.$message.error('上传图片只能是 jpg 或 png 格式!')
-					return false
-				}
-				const isLt2M = file.size / 1024 / 1024 < 2
-				if (!isLt2M) {
-					this.$message.error('上传图片大小不能超过 2MB!')
-					return false
-				}
-				// return false // (返回false不会自动上传)
-			},
-			// handlePictureCardPreview(file) {
-			//   this.dialogImageUrl = file.url
-			//   this.dialogVisible = true
-			// },
-			handleRemove(file, fileList) {
-				this.aa=fileList
-				for(var i = 0; i < this.ruleForm.jpg.length; i++){
-					if(this.ruleForm.image[i].url === file.url){
-						//      deleteImageReport(this.fileList[i].id).then(res =>{
-						//       this.$message.success('删除图片成功')
-						//      })
-						this.ruleForm.image.splice(i, 1)
-					}
-				}
-			},
-//上传图片
-			uploadImage(image){
-				var that=this
-				this.$axios({
-					url:"/upload/pic",
-					method:"get",
-					params:{
-						img:image.file
-					}
-				}).then(res=>{
-					if(res.status == 200){
-						if(res.data.status == 0)
-							that.ruleForm.image.push({name:"头像",url:res.data.url})
-					}
-					else
-						that.$message("上传失败")
-				})
-			},
-			handleExceed: function () {
-				this.$alert('请先删除选择的图片或视频，再上传  。最多上传一张', '提示', {
-					type: 'warning'
-				});
-			},
 			submitForm(formName) {
 				this.$refs[formName].validate(valid => {
 					if (valid) {
@@ -257,62 +97,6 @@
 					}
 				});
 			},
-			// 上传成功回调
-			//  handleAvatarSuccess(res, file) {
-			//    alert(this.ruleForm.jpg.length)
-			//   this.ruleForm.jpg=file
-			//  this.ruleForm.jpg.push( res.data.url)
-//    },
-			// 上传前格式和图片大小限制
-
-
-			open() {
-				this.$prompt('请在此发布您的评论', '评论', {
-					confirmButtonText: '确定',
-					cancelButtonText: '取消',
-				}).then(({ value }) => {
-					this.$axios({
-						url: ''
-					})
-					this.$message({
-						type: 'success',
-						message: '评论成功'
-					});
-				}).catch(() => {
-					this.$message({
-						type: 'info',
-						message: '取消输入'
-					});
-				});
-			},
-			copy() {
-				let clipboard = new this.Clipboard('.copyURL');
-				clipboard.on('success', e => {
-					this.$message({
-						type: 'success',
-						message: '链接已复制到剪贴板'
-					});
-					clipboard.destroy()
-				})
-				// clipboard.on('error', e => {
-				//   // 不支持复制
-				//   console.log('该浏览器不支持自动复制')
-				//   // 释放内存
-				//   clipboard.destroy()
-				// })
-			},
-			getURL() {
-				this.url = location.href
-			},
-			onSubmit() {
-				if(this.form.comment === ''){
-					alert('评论内容不能为空')
-					event.preventDefault();
-				}
-				else{
-					this.commentVisible = false;
-				}
-			}
 		}}
 
 </script>
